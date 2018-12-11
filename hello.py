b1b0beed85407b15a56502d8b2f95d48c0b1e326
wataru-kurashima <wataru-kurashima@hands-lab.com>
2018-12-11 13:10:00 +0900

    template with json & pylint

--- hello.py
+++ hello.py
@@ -2,21 +2,26 @@ import responder
 
 api = responder.API()
 
+
 @api.route("/")
 def hello_world(req, resp):
     resp.text = "hello, world!"
+
 
 @api.route("/hello/{who}")
 def hello_to_who(req, resp, *, who):
     resp.text = f"hello, {who}!"
 
+
 @api.route("/hello/{who}/json")
 def hello_to_who_json(req, resp, *, who):
     resp.media = {"hello": who}
 
+
 @api.route("/hello/{who}/html")
 def hello_html(req, resp, *, who):
-    resp.content = api.template('hello.html', who=who)
+    resp.content = api.template('hello.json', who=who)
+
 
 if __name__ == "__main__":
     api.run()
